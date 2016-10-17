d133653fd644c6ac338c62f1f832af1922f00175
Steams <radclifferobs@gmail.com>
2016-10-17 01:55:43 -0500

    Lists ui initial layouts

--- app/src/main/java/com/example/steams/a10bands/MainActivity.java
+++ app/src/main/java/com/example/steams/a10bands/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.steams.a10bands;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    String[] words = {"hey","yo"};
+    public ListView budgetsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        budgetsList = (ListView)findViewById(R.id.budgets_list);
+        budgetsList.setAdapter(
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        words)
+        );
     }
 }
